ba6e18b052fbbb4e6a06411cad677d477e3f2700
91086859 <32179176+91086859@users.noreply.github.com>
2019-08-20 04:50:37 -0700

    Create Favorites2.java

---
+++ programming-practices/1.5/Favorites2.java
@@ -0,0 +1,15 @@
+public class Favorites2
+{
+    //-------------------------------------------------------------------
+    //  Prints some information about myself, but a bit more complex.
+    //-------------------------------------------------------------------
+    public static void main(String[] args)
+    {
+        System.out.println("My name is \"The Machine\".");
+        System.out.println("My birthday is 02\\10\\2004.");
+        System.out.println("My hobbies are programming and talking to my buds.");
+        System.out.println("My favorite book is 'The Communist Manifesto.'");
+        System.out.println("My favorite movie is \"Fred 3: Camp Fred\", the definitely not worst movie ever made.");
+        // Asks to put a tab between the label and its information in the prompt, but I still don't know what the label is.
+    }
+}
