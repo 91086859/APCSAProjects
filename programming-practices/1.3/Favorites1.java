308b4bfd31d07f381d1da9e85bcfc3a2c14d0279
91086859 <32179176+91086859@users.noreply.github.com>
2019-08-20 04:38:46 -0700

    Create Favorites1.java

---
+++ programming-practices/1.3/Favorites1.java
@@ -0,0 +1,15 @@
+public class Favorites1
+{
+    //-------------------------------------------
+    //  Prints some information about myself.
+    //-------------------------------------------
+    public static void main(String[] args)
+    {
+        System.out.println("My name is Kadin H.");
+        System.out.println("My birthday is February 10th.");
+        System.out.println("My hobbies are programming and talking to my buds.");
+        System.out.println("My favorite book is The Communist Manifesto.");
+        System.out.println("My favorite movie is Fred 3: Camp Fred, the definitely not worst movie ever made.");
+        // Asks to label in the prompt, but I don't know what that means???
+    }
+}
