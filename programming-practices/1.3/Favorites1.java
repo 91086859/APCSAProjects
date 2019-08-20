aef5d3dce3025e43637d161de40e4f6f6bfb9e46
91086859 <32179176+91086859@users.noreply.github.com>
2019-08-20 04:51:12 -0700

    Update Favorites1.java

--- programming-practices/1.3/Favorites1.java
+++ programming-practices/1.3/Favorites1.java
@@ -5,8 +5,8 @@ public class Favorites1
     //-------------------------------------------
     public static void main(String[] args)
     {
-        System.out.println("My name is Kadin H.");
-        System.out.println("My birthday is February 10th.");
+        System.out.println("My name is Kuhdeen Hike.");
+        System.out.println("My birthday is February 10th, 2004.");
         System.out.println("My hobbies are programming and talking to my buds.");
         System.out.println("My favorite book is The Communist Manifesto.");
         System.out.println("My favorite movie is Fred 3: Camp Fred, the definitely not worst movie ever made.");
