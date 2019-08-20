5b6df4c05fa7edb412a03f35b67ccb1d5d1e79e8
91086859 <32179176+91086859@users.noreply.github.com>
2019-08-20 04:46:18 -0700

    Create Quotable.java

---
+++ programming-practices/1.4/Quotable.java
@@ -0,0 +1,11 @@
+public class Quotable
+{
+    //---------------------------------
+    //  Simple quote from a source.
+    //---------------------------------
+    public static void main(String[] args)
+    {
+        System.out.println("\"We live in a society that only appreciates popular, attractive, or aesthetically pleasing people, places, and things.\"");
+        System.out.println("Source: https://www.insider.com/meaning-we-live-in-a-society-mouse-meme-2018-5");
+    }
+}
