94966aa677b084144000aa01b5772a637c2b6a8e
Kuhdeen Hike <32179176+91086859@users.noreply.github.com>
2019-11-07 05:55:19 -0800

    Create ScannerArrays.java

---
+++ programming-practices/6.0/ScannerArrays.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+import java.util.Scanner;
+public class ScannerArrays 
+{
+    public static void main(String args[]) 
+    {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("How many numbers are you putting in?");
+        int[] nums = new int[scan.nextInt()];
+        System.out.println("Enter a group of numbers, however much you put in:");
+    
+        for(int i = 0; i < nums.length; i++) 
+        {
+            nums[i] = scan.nextInt();
+        }
+        System.out.println();  
+        System.out.println("Your numbers are: " + Arrays.toString(nums));
+    } // end main method
+} // end class ScannerArrays
