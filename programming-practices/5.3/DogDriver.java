d5fe4e74d995d0d91bb12dcdba1a781772203407
Kuhdeen Hike <32179176+91086859@users.noreply.github.com>
2019-11-14 13:36:57 -0800

    Update DogDriver.java

--- programming-practices/5.3/DogDriver.java
+++ programming-practices/5.3/DogDriver.java
@@ -19,27 +19,27 @@ public class DogDriver
         if(dog2.compareTo(dog1) == 0)
         {
             System.out.println(dog2.getName() + " is the same age as " + dog1.getName() + ".");
-        }
+        } // end dog2 compare dog1 conditional (same)
         else if(dog2.compareTo(dog1) == 1)
         {
             System.out.println(dog2.getName() + " is older than " + dog1.getName() + ".");
-        }
+        } // end dog2 compare dog1 conditional (older)
         else
         {
             System.out.println(dog2.getName() + " is younger than " + dog1.getName() + ".");
-        }
+        } // end dog2 compare dog1 conditional (younger)
 
         if(dog1.compareTo(dog3) == 0)
         {
             System.out.println(dog1.getName() + " is the same age as " + dog3.getName() + ".");
-        }
+        } // end dog1 compare dog3 conditional (same)
         else if(dog1.compareTo(dog3) == 1)
         {
             System.out.println(dog1.getName() + " is older than " + dog3.getName() + ".");
-        }
+        } // end dog1 compare dog3 conditional (older)
         else
         {
             System.out.println(dog1.getName() + " is younger than " + dog3.getName() + ".");
-        }
+        } // end dog1 compare dog3 conditional (younger)
     } // end main method
 } // end class DogDriver
