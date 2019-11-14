94ef46d95be761216b9d5fb61aa7a5e574fe1e08
Kuhdeen Hike <32179176+91086859@users.noreply.github.com>
2019-11-14 13:39:09 -0800

    Update ScannerArraysLimited.java

--- programming-practices/6.0Extra/ScannerArraysLimited.java
+++ programming-practices/6.0Extra/ScannerArraysLimited.java
@@ -12,7 +12,7 @@ public class ScannerArraysLimited
         for(int i = 0; i < nums.length; i++) 
         {
             nums[i] = scan.nextInt();
-        }
+        } // end nums iterator
         System.out.println(); 
         int currentIndex = -1;
         int size = 5;
@@ -23,21 +23,21 @@ public class ScannerArraysLimited
             {
                 currentIndex++;
                 temp[currentIndex] = nums[i];
-            }
+            } // end between 0 and 50 conditional
             else
             {
                 size--;
-            }
-        }
+            } // end NOT between 0 and 50 conditional
+        } // end nums iterator #2
         int[] finalArr = new int[size];
         for(int i = 0; i < finalArr.length; i++)
         {
             finalArr[i] = temp[i];
-        }
+        } // end finalArr iterator
         if(size < 5)
         {
             System.out.println("One or more of the numbers you inputted were not between 0 and 50. They have been removed.");
-        }
+        } // end size less than 5 conditional
         System.out.println("Your numbers are: " + Arrays.toString(finalArr));
     } // end main method
 } // end class ScannerArraysLimited
