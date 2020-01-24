5e5841da1006ea940acec315b90890387b162a51
Kuhdeen Hike <32179176+91086859@users.noreply.github.com>
2020-01-24 13:45:47 -0800

    Create CoinDriver.java

---
+++ programming-practices/7.0/CoinDriver.java
@@ -0,0 +1,9 @@
+public class CoinDriver 
+{
+	public static void main(String[] args) 
+	{
+	    MonetaryCoin[] coins = {new MonetaryCoin(6.6), new MonetaryCoin(3.519), new MonetaryCoin(7.77)};
+		
+		System.out.println(new MonetaryCoin(10).addCoins(coins).getValue());
+	}
+}
