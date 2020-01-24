caf67f20aba36b96c1caec8ab8b4fd155e53b70c
Kuhdeen Hike <32179176+91086859@users.noreply.github.com>
2020-01-24 13:46:19 -0800

    Update CoinDriver.java

--- programming-practices/7.0/CoinDriver.java
+++ programming-practices/7.0/CoinDriver.java
@@ -5,5 +5,5 @@ public class CoinDriver
 	    MonetaryCoin[] coins = {new MonetaryCoin(6.6), new MonetaryCoin(3.519), new MonetaryCoin(7.77)};
 		
 		System.out.println(new MonetaryCoin(10).addCoins(coins).getValue());
-	}
-}
+	} // end main method
+} // end class CoinDriver
