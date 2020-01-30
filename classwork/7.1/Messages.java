ed1fbd6ac484dfc5474f8dd0ca0b719122d131dd
Kuhdeen Hike <32179176+91086859@users.noreply.github.com>
2020-01-30 13:18:41 -0800

    Create Messages.java

---
+++ classwork/7.1/Messages.java
@@ -0,0 +1,13 @@
+public class Messages {
+    public static void main(String args[]) {
+      System.out.println("Kuhdeen Hike");
+      System.out.println();
+      System.out.println();
+      Thought thinking = new Thought();
+      Advice speaking = new Advice();
+      
+      thinking.message();
+      
+      speaking.message();
+    }
+}
