580b92ddec983ad3d6d7fc58f12c15cd09fe10b4
Kuhdeen Hike <32179176+91086859@users.noreply.github.com>
2020-01-31 13:26:40 -0800

    Create BlueFootedBooby.java

---
+++ classwork/7.5/BlueFootedBooby.java
@@ -0,0 +1,23 @@
+public class BlueFootedBooby extends Pet
+{
+
+    public BlueFootedBooby(String name, boolean owner, int age, double weight, String food)
+    {
+        super(name, owner, age, weight, food);
+    }
+
+    public int monthlyFeeding()
+    {
+        return 35902;
+    }
+
+    public String Speak()
+    {
+        return "bruh";
+    }
+
+    public String toString()
+    {
+        return super.toString();
+    }
+}
