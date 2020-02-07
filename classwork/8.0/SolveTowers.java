0e6837bc6d594017cffe3c6b1e04dd3ff395562e
Kuhdeen Hike <32179176+91086859@users.noreply.github.com>
2020-02-07 13:21:09 -0800

    Create SolveTowers.java

---
+++ classwork/8.0/SolveTowers.java
@@ -0,0 +1,9 @@
+public class SolveTowers 
+{
+    public static void main(String args[])
+    {
+      System.out.println("Kuhdeen Hike");
+      TowersOfHanoi towers = new TowersOfHanoi(4);
+      towers.solve();
+    }
+}
